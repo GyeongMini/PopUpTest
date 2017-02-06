1dcc4e0cec5e90c64180e43541ce07c636113b94
GyeongMini <dlfjsskawk00@naver.com>
2017-02-06 23:15:14 +0900

    2017.02.06
    버튼을 눌렀을대 팝업이 나오게 하도록 하는 연습!

---
+++ app/src/main/java/com/example/user/popuptest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.user.popuptest;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mWidthPixels, mHeightPixels;
+    private PopupWindow pwindo;
+    Point point;
+    Button category, game;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        category = (Button)findViewById(R.id.category);
+        category.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goAddPopup();
+            }
+        });
+    }
+
+    public void goAddPopup() {
+        WindowManager w = getWindowManager();
+        Display d = w.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        d.getMetrics(metrics);
+        // since SDK_INT = 1;
+        mWidthPixels = metrics.widthPixels;
+        mHeightPixels = metrics.heightPixels;
+
+        // 상태바와 메뉴바의 크기를 포함해서 재계산
+        if (Build.VERSION.SDK_INT >= 14 && Build.VERSION.SDK_INT < 17)
+            try {
+                mWidthPixels = (Integer) Display.class.getMethod("getRawWidth").invoke(d);
+                mHeightPixels = (Integer) Display.class.getMethod("getRawHeight").invoke(d);
+            } catch (Exception ignored) {
+            }
+        // 상태바와 메뉴바의 크기를 포함
+        if (Build.VERSION.SDK_INT >= 17)
+            try {
+                Point realSize = new Point();
+                Display.class.getMethod("getRealSize", Point.class).invoke(d, realSize);
+                mWidthPixels = realSize.x;
+                mHeightPixels = realSize.y;
+            } catch (Exception ignored) {
+            }
+
+        LayoutInflater inflater = (LayoutInflater) MainActivity.this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View layout = inflater.inflate(R.layout.popup_layout, (ViewGroup) findViewById(R.id.popup_element));
+       // 팝업 창의 사이즈 조절
+        pwindo = new PopupWindow(layout, mWidthPixels - 100, mHeightPixels - 1000, true);
+        pwindo.showAtLocation(layout, Gravity.CENTER, 0, 0);
+        game = (Button)layout.findViewById(R.id.game);
+    }
+}
+
